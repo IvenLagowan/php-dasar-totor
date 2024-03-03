c862b28111602c2c2b38b451fa1f06361a55f5af
chua lagowan <cuaocuq@gmail.com>
2024-03-02 23:21:07 -0800

    new flder

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+
 # php-dasar-totor
 Bahan materi dari YouTube 
+
+## link youtube : https://youtu.be/XTrU0GzMfCk?si=TZvvF67h6vWE9YU-&t=672
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+//  ini adalah komentar
+// ini juga komentar
+
+
+/*
+    ini juga komentar
+    ini juga
+*/
+$nama = "Chua LA";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Hello,  selamat datang <?php echo $nama; ?></h1>
+    <p><?php echo "Ini adalah paragraf";?></p>
+</body>
+</html>
